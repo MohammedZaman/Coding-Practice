4721d48947ea00b04c3a316279686e7052374ab0
mohammedzaman <mohamedsaifuz@gmail.com>
2022-05-25 08:13:37 +0100

    Two Sums Basic Working Version

---
+++ src/com/two/sum/Main.java
@@ -0,0 +1,30 @@
+package com.two.sum;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int target = 5;
+	int [] nums = new int[]{2,4,6,7,8,3,5};
+	System.out.println("Index of numbers that add up to target: " + target);
+    System.out.println(Arrays.toString(twoSum(nums,target)));
+
+
+    }
+
+    private static int[] twoSum(int[] nums, int target){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i; j < nums.length; j++){
+               if(nums[i] + nums[j] == target){
+                   return new int[]{i,j};
+               }
+            }
+        }
+        return new int[]{};
+    }
+
+
+
+
+}
